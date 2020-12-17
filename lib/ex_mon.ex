f951cf122f07f270d749d861f6ba632e96c445f1
Seveshy <danielalvesmajor@outlook.com>
2020-12-17 16:40:11 -0300

    Imprimindo estado do jogo

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,8 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
+  alias ExMon.Game.Status
 
-  @computer_name = "Robotinik"
+  @computer_name "Robotinik"
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_avg, move_rnd, move_heal)
@@ -11,5 +12,7 @@ defmodule ExMon do
     @computer_name
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
